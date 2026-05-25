347f1f9954dcc38e2a5eb2fa23923fca2dc95bc3
iymen mohamed <liocon02@gmail.com>
2026-05-25 13:40:59 -0500

    Add Spring Boot starter structure

---
+++ src/test/java/com/iymen/campus_room_booking/CampusRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iymen.campus_room_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
