c0776354a1c90e794f8a420945c0c7f2b056b917
iymen mohamed <liocon02@gmail.com>
2026-05-26 09:23:52 -0500

    Add health check endpoint

---
+++ src/main/java/com/iymen/campusroombooking/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.iymen.campusroombooking.controller;
+
+import javax.management.MXBean;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("api/health")
+    public String health() {
+        return "Campus Room Booking API is running.";
+    }
+}
