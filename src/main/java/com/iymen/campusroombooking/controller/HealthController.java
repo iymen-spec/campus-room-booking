361228a0a40662c5c38faaa0097e7e76871ae170
iymen mohamed <liocon02@gmail.com>
2026-05-26 09:27:19 -0500

    Clean up health endpoint

--- src/main/java/com/iymen/campusroombooking/controller/HealthController.java
+++ src/main/java/com/iymen/campusroombooking/controller/HealthController.java
@@ -1,14 +1,12 @@
 package com.iymen.campusroombooking.controller;
 
-import javax.management.MXBean;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    
-    @GetMapping("api/health")
+
+    @GetMapping("/api/health")
     public String health() {
         return "Campus Room Booking API is running.";
     }
